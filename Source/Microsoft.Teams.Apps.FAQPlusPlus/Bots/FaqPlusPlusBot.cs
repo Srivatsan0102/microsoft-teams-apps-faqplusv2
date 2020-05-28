b215a5d0c3eada3cbb9032e37cb26430bae44d14
Srivatsan0102 <63128678+Srivatsan0102@users.noreply.github.com>
2020-05-28 10:03:45 -0700

    Making changes to multikb

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Bots/FaqPlusPlusBot.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Bots/FaqPlusPlusBot.cs
@@ -74,6 +74,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Bots
         /// </summary>
         private const string ChangeStatus = "change status";
 
+        private string question;
         /// <summary>
         /// Represents a set of key/value application configuration properties for FaqPlusPlus bot.
         /// </summary>
@@ -844,22 +845,31 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Bots
 
                     var knowledgebaseId1 = await this.configurationProvider.GetSavedEntityDetailAsync(ConfigurationEntityTypes.KnowledgeBaseId).ConfigureAwait(false);
                     await this.configurationProvider.UpsertEntityAsync(knowledgebaseId1, ConfigurationEntityTypes.MainKnowledgeBase).ConfigureAwait(false);
-                    await turnContext.SendActivityAsync(MessageFactory.Text("You've selected KB1!")).ConfigureAwait(false); // To be modified to a card.
+                    // await turnContext.SendActivityAsync(MessageFactory.Text("You've selected KB1!")).ConfigureAwait(false); // To be modified to a card.
+                    await this.GetQuestionAnswerReplyAsync(turnContext, this.question).ConfigureAwait(false);
                     break;
                 case Constants.KB2:
                     this.logger.LogInformation("You have selected KB 2");
 
                     var knowledgebaseId2 = await this.configurationProvider.GetSavedEntityDetailAsync(ConfigurationEntityTypes.KnowledgeBaseId2).ConfigureAwait(false);
                     await this.configurationProvider.UpsertEntityAsync(knowledgebaseId2, ConfigurationEntityTypes.MainKnowledgeBase).ConfigureAwait(false);
-                    await turnContext.SendActivityAsync(MessageFactory.Text("You've selected KB2!")).ConfigureAwait(false); // To be modified to a card.
+                    // await turnContext.SendActivityAsync(MessageFactory.Text("You've selected KB2!")).ConfigureAwait(false); // To be modified to a card.
+                    await this.GetQuestionAnswerReplyAsync(turnContext,this.question).ConfigureAwait(false);
                     break;
                 default:
                     this.logger.LogInformation("Sending input to QnAMaker");
                     Attachment newCard = new MultipleKBCard(this.configurationProvider, this.qnaMakerClient).GetCard("Select your KB bot from the following choices!");
                     await turnContext.SendActivityAsync(MessageFactory.Attachment(newCard)).ConfigureAwait(false);
-                    await this.GetQuestionAnswerReplyAsync(turnContext, text).ConfigureAwait(false);
+                    this.question = this.ReturnUserQuestion(text);
+                    // await this.GetQuestionAnswerReplyAsync(turnContext, text).ConfigureAwait(false);
                     break;
             }
+        }
+
+        private string ReturnUserQuestion(string text)
+        {
+            string question = text;
+            return question;
         }
 
         /// <summary>
