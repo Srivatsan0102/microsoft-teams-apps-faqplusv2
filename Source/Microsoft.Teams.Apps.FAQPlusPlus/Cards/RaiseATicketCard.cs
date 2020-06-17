60507e924509ca4d31413dfb6756232bf27be705
Srivatsan0102 <63128678+Srivatsan0102@users.noreply.github.com>
2020-06-17 15:57:26 -0700

    Checking for servicenow integration

--- Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/RaiseATicketCard.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus/Cards/RaiseATicketCard.cs
@@ -8,6 +8,7 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
     using System.Collections.Generic;
     using AdaptiveCards;
     using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+    using Microsoft.AspNetCore.Rewrite.Internal.IISUrlRewrite;
     using Microsoft.Bot.Schema;
     using Microsoft.Teams.Apps.FAQPlusPlus.Common.Models;
     using Microsoft.Teams.Apps.FAQPlusPlus.Properties;
@@ -17,11 +18,8 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
     /// </summary>
     public static class RaiseATicketCard
     {
-        /// <summary>
-        /// This method will construct the card for raise a ticket bot menu.
-        /// </summary>
-        /// <returns>Raise a Ticket card.</returns>
-        public static Attachment GetCard()
+
+        public static string getURL()
         {
             Uri uri = new Uri("http://www.tcs.com");
             var serviceNowText = $@"<html>
@@ -30,13 +28,23 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Cards
                                        <div><a href = {uri.AbsoluteUri} > Click here to access the service now portal! </a></div>
                                        </body>
                                        </html>";
+
+            return serviceNowText.Trim();
+        }
+
+        /// <summary>
+        /// This method will construct the card for raise a ticket bot menu.
+        /// </summary>
+        /// <returns>Raise a Ticket card.</returns>
+        public static Attachment GetCard()
+        {
             AdaptiveCard raiseATicketCard = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0))
             {
                 Body = new List<AdaptiveElement>
                 {
                     new AdaptiveTextBlock
                     {
-                        Text = serviceNowText.Trim(),
+                        Text = getURL(),
                         Wrap = true,
                     },
                 },
